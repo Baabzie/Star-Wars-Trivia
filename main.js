b61aaa8c68ed6dd9f5063239807c68991c4da3bc
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-03-08 12:06:12 +0100

    crated class and selects

---
+++ main.js
@@ -0,0 +1,13 @@
+class Chaaracter {
+    constructor(name, gender, height, mass, hairColor, skinColor, eyeColor, movies, pictureURL) {
+        this.name = name;
+        this.gender = gender;
+        this.height = height;
+        this.mass = mass;
+        this.hairColor = hairColor;
+        this.skinColor = skinColor;
+        this.eyeColor = eyeColor;
+        this.movies = movies;
+        this.pictureURL = pictureURL;
+    }
+}
